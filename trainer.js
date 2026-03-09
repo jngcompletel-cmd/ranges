895a69748d7cafb14b3ea7bf1a977424caab3b9e
jngcompletel-cmd <jngcompletel@gmail.com>
2026-03-09 16:19:06 +0100

    Add files via upload

--- trainer.js
+++ trainer.js
@@ -2,7 +2,10 @@ const ranks=["A","K","Q","J","T","9","8","7","6","5","4","3","2"]
 
 let gridHands=[]
 let ranges={}
+let errorsMemory={}
+
 let currentSpot="BTN_open_25bb"
+let mode="normal"
 
 let sessionHands=[]
 let retryHands=[]
@@ -12,7 +15,7 @@ let index=0
 let total=0
 let correct=0
 
-const sessionSize=20
+let sessionSize=20
 
 function generateHands(){
 
@@ -23,21 +26,13 @@ for(let j=0;j<13;j++){
 let hand
 
 if(i===j){
-
 hand=ranks[i]+ranks[j]
-
 }
-
 else if(i<j){
-
 hand=ranks[i]+ranks[j]+"s"
-
 }
-
 else{
-
 hand=ranks[j]+ranks[i]+"o"
-
 }
 
 gridHands.push(hand)
@@ -66,11 +61,9 @@ alert("Range chargée")
 
 startQuiz()
 
-}
+}catch{
 
-catch{
-
-alert("Erreur dans le JSON")
+alert("Erreur JSON")
 
 }
 
@@ -83,8 +76,122 @@ reader.readAsText(file)
 function changeSpot(){
 
 currentSpot=document.getElementById("spotSelector").value
-
 startQuiz()
+
+}
+
+function changeMode(){
+
+mode=document.getElementById("modeSelector").value
+startQuiz()
+
+}
+
+function changeSize(){
+
+sessionSize=parseInt(document.getElementById("sizeSelector").value)
+startQuiz()
+
+}
+
+function getRangeHands(){
+
+let spot=ranges[currentSpot]
+
+if(!spot) return []
+
+let all=[]
+
+if(spot.raise) all=all.concat(spot.raise)
+if(spot.shove) all=all.concat(spot.shove)
+if(spot.call) all=all.concat(spot.call)
+if(spot.var) all=all.concat(spot.var)
+
+return [...new Set(all)]
+
+}
+
+function getBorderHands(){
+
+let range=getRangeHands()
+
+let borders=[]
+
+for(let hand of range){
+
+let r1=hand[0]
+let r2=hand[1]
+
+let idx1=ranks.indexOf(r1)
+let idx2=ranks.indexOf(r2)
+
+if(idx1+1<ranks.length){
+borders.push(ranks[idx1+1]+r2+hand.slice(2))
+}
+
+if(idx2+1<ranks.length){
+borders.push(r1+ranks[idx2+1]+hand.slice(2))
+}
+
+}
+
+return [...new Set(borders)]
+
+}
+
+function buildPool(){
+
+let pool=[]
+
+if(mode==="range"){
+pool=getRangeHands()
+}
+else if(mode==="border"){
+pool=getBorderHands()
+}
+else{
+pool=[...gridHands]
+}
+
+if(pool.length<sessionSize){
+pool=[...gridHands]
+}
+
+return pool
+
+}
+
+function addErrorWeight(pool){
+
+let weighted=[...pool]
+
+for(let hand of pool){
+
+if(errorsMemory[hand]){
+
+for(let i=0;i<errorsMemory[hand];i++){
+weighted.push(hand)
+}
+
+}
+
+}
+
+return weighted
+
+}
+
+function shuffle(array){
+
+for(let i=array.length-1;i>0;i--){
+
+let j=Math.floor(Math.random()*(i+1))
+
+let temp=array[i]
+array[i]=array[j]
+array[j]=temp
+
+}
 
 }
 
@@ -95,15 +202,25 @@ correct=0
 index=0
 retryHands=[]
 
-sessionHands=[]
+let pool=buildPool()
 
-for(let i=0;i<sessionSize;i++){
+pool=addErrorWeight(pool)
 
-sessionHands.push(
-gridHands[Math.floor(Math.random()*gridHands.length)]
-)
+shuffle(pool)
+
+let unique=[]
+
+for(let hand of pool){
+
+if(!unique.includes(hand)){
+unique.push(hand)
+}
+
+if(unique.length===sessionSize) break
 
 }
+
+sessionHands=unique
 
 nextHand()
 
@@ -139,13 +256,9 @@ let r2=hand[1]
 let type=hand[2]
 
 if(order.indexOf(r1) < order.indexOf(r2)){
-
 return r1+r2+type
-
 }else{
-
 return r2+r1+type
-
 }
 
 }
@@ -189,6 +302,8 @@ correct++
 
 retryHands.push(currentHand)
 
+errorsMemory[currentHand]=(errorsMemory[currentHand]||0)+1
+
 }
 
 index++
@@ -212,7 +327,7 @@ document.getElementById("accuracy").innerText=acc
 
 function endQuiz(){
 
-document.getElementById("hand").innerText="Quiz terminé"
+document.getElementById("hand").innerText="Session terminée"
 
 alert(
 "Score : "+correct+" / "+total+
@@ -220,16 +335,14 @@ alert(
 )
 
 if(retryHands.length>0){
-
 document.getElementById("retryBtn").style.display="inline"
-
 }
 
 }
 
 function retryErrors(){
 
-sessionHands=[...retryHands]
+sessionHands=[...new Set(retryHands)]
 
 retryHands=[]
 
